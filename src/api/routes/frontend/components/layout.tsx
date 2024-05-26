1422e666924b8dccf7a239f2d4a77e9b507498ad
ToastedToast <hey@toasted.dev>
2024-05-26 20:55:19 +0800

    improve navbar

--- src/api/routes/frontend/components/layout.tsx
+++ src/api/routes/frontend/components/layout.tsx
@@ -1,23 +1,34 @@
 import type { Child } from "hono/jsx";
+import { useRequestContext } from "hono/jsx-renderer";
+
+const links = [{ href: "/lb", text: "Leaderboard" }];
 
 function Navbar() {
+  const c = useRequestContext();
+  const { pathname } = new URL(c.req.url);
+
   return (
     <header className="border-b border-blue-900/80">
-      <div class="container px-6 py-3 flex items-center justify-between">
+      <div class="container flex items-center justify-between px-6 py-3">
         <nav class="flex items-center gap-4">
           <a href="/">
             <p class="text-xl font-bold tracking-tighter text-blue-500 transition-all hover:opacity-80">
               Graphical
             </p>
           </a>
-          <a href="/lb">
-            <p class="text-sm transition-all hover:opacity-80">
-              Leaderboard
-            </p>
-          </a>
+          {links.map((link) => (
+            <a
+              key={link.href}
+              href={link.href}
+              class={`text-sm text-slate-50/50 transition-all hover:!text-slate-50/80 ${pathname === link.href ? "font-medium !text-slate-50" : ""}`}
+            >
+              {link.text}{" "}
+            </a>
+          ))}
         </nav>
         <div></div>
       </div>
+      <div class="font-medium" />
     </header>
   );
 }
